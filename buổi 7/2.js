c8cdd453d33196101236d9d3ca1bc1a3c25b7953
HongHanHannah <109467736+HongHanHannah@users.noreply.github.com>
2022-12-11 21:20:25 +0700

    buoi 7

---
+++ buổi 7/2.js
@@ -0,0 +1,25 @@
+// function student(name , age, address,) {
+//     console.log(student)
+// }
+
+// student.name(Hân)
+// student.age(14)
+// student.address('Diên Khánh')
+
+var smartPhones = [ 
+    { name: 'iphone', price: 649 }, 
+    { name: 'Galaxy S6', price: 576 }, 
+    { name: 'Galaxy Note 5', price: 489 } 
+];
+
+smartPhones.forEach(function (phone){
+   console.log(phone.price);
+});
+
+const es6_foo = (x,y,z) =>{
+    console.log(x,y,z)
+}
+foo([x,y,z])
+
+
+
